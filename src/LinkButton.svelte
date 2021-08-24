b59436e66c9a199133d87a3bc2c5db266d793cf3
Syuparn <s.hello.spagetti@gmail.com>
2021-08-24 09:33:27 +0900

    add next/back button

---
+++ src/LinkButton.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+    export let text: string;
+    export let href: string;
+</script>
+
+<p><a href={href}>{text}</a></p>
+
+<style>
+    p {
+        border-width: 0;
+        border-radius: 0.3em;
+        padding: 0.4em;
+        margin-left: 4em;
+        background-color: #17262b;
+    }
+
+    p:hover {
+        background-color: #c0d32b;
+    }
+
+    p:hover a {
+        background-color: #c0d32b;
+        color: #17262b;
+    }
+
+    a {
+        color: #c0d32b;
+        font-size: 1.1em;
+    }
+
+    a:hover {
+	    text-decoration: none;
+    }
+</style>
+
